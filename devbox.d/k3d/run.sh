e5ad755aecc45db7a9f5f465b1b4f49c46d93928
gamezftw <gamezftw@yahoo.com>
2025-12-23 22:36:23 +0100

    refactor: move helm to bin

--- devbox.d/k3d/run.sh
+++ devbox.d/k3d/run.sh
@@ -5,8 +5,8 @@ k3d cluster create --config $DEVBOX_CONFIG_DIR/k3d/config.yaml
 kubectl create namespace kafka
 kubectl config set-context --current --namespace=kafka
 kubectl create -f 'https://strimzi.io/install/latest?namespace=kafka' -n kafka
-$DEVBOX_PROJECT_ROOT/helm/linux-amd64/helm repo add kafbat-ui https://kafbat.github.io/helm-charts
-$DEVBOX_PROJECT_ROOT/helm/linux-amd64/helm install kafbat-ui kafbat-ui/kafka-ui -f $DEVBOX_PROJECT_ROOT/infra/helm/values.yml
+helm repo add kafbat-ui https://kafbat.github.io/helm-charts
+helm install kafbat-ui kafbat-ui/kafka-ui -f $DEVBOX_PROJECT_ROOT/infra/helm/values.yml
 kubectl apply -f $DEVBOX_PROJECT_ROOT/infra/kafka-single-node.yaml
 kubectl apply -f $DEVBOX_PROJECT_ROOT/infra/helm/ingress.yml
 
